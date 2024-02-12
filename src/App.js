fae9ffdbb7e8fc36101af557fb82c604e8641770
Sakshi Maurya <96237611+SAKSHI3MAURYA@users.noreply.github.com>
2024-02-12 18:37:01 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TicTacToe from './components/TicTacToe';
+
+export default function App() {
+
+  return (
+    <div className='container'>
+    <TicTacToe />
+    </div>
+  )
+}
+
+
+
+
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,104 @@
+import '../App.css';
+import { useState } from 'react';
+
+export default function TicTacToe() {
+  //Initialising State Variables
+  const [turn,setTurn] = useState("X");
+  const [cells,setCells] = useState(Array(9).fill(""));
+  const [winner,setWinner] = useState();
+  const [isDraw, setIsDraw] = useState(false);
+
+
+  //Defining WInning Combinations
+  const combos = {
+    across: [[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+    down: [[0, 3, 6], [1, 4, 7], [2, 5, 8]],
+    diagonal: [[0, 4, 8], [2, 4, 6]]
+  };
+
+  //Function to handle Clicks
+  const handleClick = (num) =>{
+    if (cells[num] !== "" ) return;
+
+    let arr =[...cells];
+    if(turn==="X"){
+      arr[num]= "X";
+      setTurn("O");
+    }else{
+      arr[num]="O";
+      setTurn("X");
+    }
+    checkWinner(arr);
+    setCells(arr);
+    if (!arr.includes("") && !winner) {
+      setIsDraw(true);
+    }
+  }
+
+  //Function to check for the winner
+  const checkWinner =(arr) =>{
+    for(let combo in combos){
+    combos[combo].forEach((pattern) => {
+        
+      
+      if (arr[pattern[0]] === "" || arr[pattern[1] === "" || arr[pattern[2]]] ===""){
+        //Dont return anything
+      }else if(arr[pattern[0]]===arr[pattern[1]] && arr[pattern[1]]===arr[pattern[2]]){
+        setWinner(arr[pattern[0]]);
+      }
+    });
+  }
+}
+
+  const Cell =({num}) =>{
+    const cellValue =cells[num];
+    const cellClassName = cellValue ? `cell cell-${cellValue}` : "cell";
+  return (
+    <td className={cellClassName} onClick={()=>handleClick(num)}>
+    {cellValue}
+    </td>
+  );
+  
+  }
+
+  
+
+
+  const handleReset = () =>{
+    setWinner();
+    setIsDraw(false);
+    setCells(Array(9).fill(""));
+  }; 
+
+  return (
+    <div className='container'>
+      <div className={`winner ${winner || isDraw ? "show" : ""}`}>
+        {winner ? `Winner is: ${winner}`: isDraw ? "Its a Draw" :""}
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
+      <button className="reset-button" onClick={handleReset}>New Game</button>
+    </div>
+  );
+}
+
+
+
+
